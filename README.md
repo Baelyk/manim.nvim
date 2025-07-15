319db6e85f8691758d85199a49765d3726263ce0
Baelyk <b43lyk@gmail.com>
2025-07-15 02:59:24 +0200

    Refactor to have setup function and no quality arguments

--- README.md
+++ README.md
@@ -1,26 +1,42 @@
 # manim.nvim
 
-A small plugin that creates two commands to render Manim scenes.
-- `ManimRender`: render a specified scene or all scenes (default) with specified quality
+A small plugin that creates commands to render Manim scenes. There are two families of commands:
+- `ManimRender`: render a specified scene or all (default)
     - Accepts two args:
-        - the scene name or `all scenes` (default)
-        - the render quality, defaults to `l`: `[l|m|h|p|k]`
+        - the scene name or `all` (default)
 - `ManimRenderUnderCursor`: render the scene under the cursor with specified quality
-    - Uses treesitter to find the scene name and then falls back to `ManimRender`
-    - Accepts one arg:
-        - the render quality, defaults to `l`: `[l|m|h|p|k]`
+    - Uses treesitter to find the scene name
+There exist variants of these two for each quality level: `Low`, `Medium`, `High`, `1440`, and `4k`.
 
 Example configuration using `lazy.nvim`:
 ```lua
 {
     "Baelyk/manim.nvim",
+    name = "manim.nvim",
     ft = "python",
-    config = function()
-        require("manim")
-        vim.keymap.set("n", "<leader>ll", vim.cmd.ManimRenderUnderCursor)
-        vim.keymap.set("n", "<leader>lm", function() vim.cmd.ManimRenderUnderCursor("m") end)
-        vim.keymap.set("n", "<leader>lh", function() vim.cmd.ManimRenderUnderCursor("h") end)
-        vim.keymap.set("n", "<leader>la", vim.cmd.ManimRender)
-    end
-}
+    config = true,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    keys = {
+        {
+            "<leader>ll",
+            vim.cmd.ManimRenderUnderCursorLow,
+            desc = "Render current Manim scene in low quality",
+        },
+        {
+            "<leader>lm",
+            vim.cmd.ManimRenderUnderCursorMedium,
+            desc = "Render current Manim scene in medium quality",
+        },
+        {
+            "<leader>lh",
+            vim.cmd.ManimRenderUnderCursorHigh,
+            desc = "Render current Manim scene in high quality",
+        },
+        {
+            "<leader>la",
+            vim.cmd.ManimRenderLow,
+            desc = "Render all Manim scenes in low quality",
+        },
+    },
+},
 ```
--- lua/manim.lua
+++ lua/manim.lua
@@ -1,12 +1,52 @@
--- Returns a list of the scenes in the specified buffer
+---@alias Quality "l"|"m"|"h"|"p"|"k"
+local qualities = { l = "Low", m = "Medium", h = "High", p = "1440", k = "4k" }
+
+---Wrapper around `vim.notify`
+---@param msg string
+---@param level? integer
+---@param key? string replace previous notification with this key
+local function notify(msg, level, key)
+	vim.notify(msg, level, { key = "manim.nvim-" .. (key or ""), group = "manim.nvim", annote = "manim.nvim" })
+end
+
+--- Ensure this buffer has been parsed, and get the tree
+---@param bufnr? integer the buffer for which to get the parser, defaults to the current buffer
+local function parse(bufnr)
+	local parser = vim.treesitter.get_parser(bufnr)
+	if parser then
+		return (parser:parse() or {})[1]
+	end
+	return nil
+end
+
+---@return string|nil # the scene under the cursor, if there was one
+local function scene_under_cursor()
+	-- Ensure this buffer has been parsed
+	parse()
+	-- Get the scene under the cursor by traversing up the tree looking for the first class
+	local scene = nil
+	local node = vim.treesitter.get_node()
+	while node do
+		if node:type() == "class_definition" then
+			scene = vim.treesitter.get_node_text(node:field("name")[1], 0)
+			break
+		end
+		node = node:parent()
+	end
+	return scene
+end
+
+---Returns a list of the scenes in the specified buffer
+---@param bufnr? integer the buffer for which to get the parser, defaults to the current buffer
 local function scenes_in_buf(bufnr)
 	bufnr = bufnr or 0
-	local buf_path = vim.api.nvim_buf_get_name(bufnr)
-	-- Ensure this buffer has been parsed
-	local parser = vim.treesitter.get_parser(bufnr)
-	parser:parse()
+	local parser = parse(bufnr)
+	if not parser then
+		return
+	end
 
-	local children = parser:trees()[1]:root():iter_children()
+	local children = parser:root():iter_children()
+	---@type table<string>
 	local scenes = {}
 
 	for node in children do
@@ -19,91 +59,105 @@ local function scenes_in_buf(bufnr)
 	return scenes
 end
 
-local function find(list, value)
-	for index, list_value in ipairs(list) do
-		if list_value == value then
-			return index
-		end
-	end
-	return nil
-end
-
--- Render all scenes or a specified scene
-vim.api.nvim_create_user_command("ManimRender", function(opts)
-	-- Accept specified quality or default to low and validate
-	local quality = opts.fargs[1] or "l"
-	if not (quality == "l"
-			or quality == "m"
-			or quality == "h"
-			or quality == "p"
-			or quality == "k") then
-		print("Error: Expected quality [l|m|h|p|k], not " .. quality)
-		return
-	end
-	-- Get the current buffer path
+---Run `manim render`
+---@param quality Quality
+---@param scenes table<string>
+local function render(quality, scenes)
 	local current_buffer = 0
-	local buf_path = vim.api.nvim_buf_get_name(current_buffer)
-	-- Accept specified scene and validate or default to all scenes
-	local scenes = scenes_in_buf(current_buffer)
-	local scene = opts.fargs[2]
-	if opts.fargs[1] then
-		if find(scenes, scene) then
-			scene = opts.fargs[2]
-		else
-			print("Unexpected scene " .. scene)
-			return
-		end
+	local path = vim.api.nvim_buf_get_name(current_buffer)
+
+	---@type string
+	local scene_name
+	if #scenes == 1 then
+		scene_name = scenes[1]
 	else
-		scene = "all scenes"
+		scene_name = #scenes .. " scenes"
 	end
-	-- Run manim
+
 	local on_exit = function(out)
 		if out.code ~= 0 then
-			print("Error: ", out.stdout)
+			notify(out.stdout, vim.log.levels.ERROR)
 		else
-			print("Rendered " .. scene)
+			notify("Rendered " .. scene_name, nil, "render")
 		end
 	end
-	local command = { "manim", "render", "--quality", quality, buf_path }
-	if scene == "all scenes" then
-		for _, s in ipairs(scenes) do
-			table.insert(command, s)
-		end
-	else
-		table.insert(command, scene)
+	local command = { "manim", "render", "--quality", quality, path }
+	for _, s in ipairs(scenes) do
+		table.insert(command, s)
 	end
-	vim.system(
-		command, {}, on_exit
-	)
-	print("Rendering " .. scene .. "...")
-end, {
-	nargs = "*",
-})
+	vim.system(command, {}, on_exit)
 
--- Render the scene under the cursor
-vim.api.nvim_create_user_command("ManimRenderUnderCursor", function(opts)
-	-- Ensure this buffer has been parsed
-	local current_buffer = 0
-	local parser = vim.treesitter.get_parser(current_buffer)
-	parser:parse()
-	-- Get the scene under the cursor by traversing up the tree looking for the first class
-	local scene
-	local node = vim.treesitter.get_node()
-	while node do
-		if node:type() == "class_definition" then
-			scene = vim.treesitter.get_node_text(node:field("name")[1], current_buffer)
-			break
-		end
-		node = node:parent()
-	end
-	if not scene then
-		print("Scene not found")
-		return
-	end
-	vim.cmd.ManimRender(opts.fargs[1] or "l", scene)
-end, {
+	notify("Rendering " .. scene_name .. "...", nil, "render")
+end
+
+---Completion options for the ManimRender<Quality> commands
+local manim_render_completion = {
 	nargs = "?",
-	complete = function(_, _, _)
-		return { "l", "m", "h", "p", "k" }
+	complete = function()
+		local completions = scenes_in_buf()
+		if not completions then
+			return { "all" }
+		end
+		table.insert(completions, "all")
+		return completions
+	end,
+}
+
+---Render all scenes or a specified scene
+---@param quality Quality
+local function manim_render(quality)
+	---@param opts vim.api.keyset.create_user_command.command_args
+	return function(opts)
+		-- Accept specified scene and validate or default to all scenes
+		local buf_scenes = scenes_in_buf()
+		if not buf_scenes then
+			notify("Unable to get scenes in the buffer", vim.log.levels.ERROR)
+			return
+		end
+		local scenes
+		if opts.fargs[2] then
+			if vim.tbl_contains(scenes, opts.fargs[2]) then
+				scenes = { opts.fargs[2] }
+			else
+				notify("Unexpected scene " .. opts.fargs[2], vim.log.levels.ERROR)
+				return
+			end
+		else
+			scenes = buf_scenes
+		end
+
+		-- Run manim
+		render(quality, scenes)
 	end
-})
+end
+
+---Render the scene under the cursor
+---@param quality Quality
+local function manim_render_under_cursor(quality)
+	return function()
+		-- Get the scene under the cursor
+		local scene = scene_under_cursor()
+		if not scene then
+			notify("No scene under cursor", vim.log.levels.WARN)
+			return
+		end
+
+		render(quality, { scene })
+	end
+end
+
+local M = {}
+
+function M.setup()
+	-- Render all scenes or a specified scene
+	for quality, name in pairs(qualities) do
+		vim.api.nvim_create_user_command("ManimRender" .. name, manim_render(quality), manim_render_completion)
+	end
+
+	-- Render the scene under the cursor
+	for quality, name in pairs(qualities) do
+		vim.api.nvim_create_user_command("ManimRenderUnderCursor" .. name, manim_render_under_cursor(quality), {})
+	end
+end
+
+return M
